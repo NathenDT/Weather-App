5c01a7435d994b8564b39847b79c8fd8468a982d
Nathen dela Torre <35028757+N20Code@users.noreply.github.com>
2021-11-02 00:51:41 -1000

    Initialized

---
+++ src/components/Head.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import { Helmet } from 'react-helmet'
+
+import Weather from '../utils/interfaces/Weather'
+
+type Props = {
+  weather: Weather
+}
+
+export default function Head({ weather }: Props): JSX.Element {
+  const
+    [iconID, setIconID] = useState('01d'),
+    [title, setTitle] = useState('Weather App')
+
+  useEffect(() => {
+    if(!Boolean(weather.name)) return
+
+    setIconID(weather.weather[0].icon)
+    setTitle(`${weather.name} Weather`)
+  }, [])
+
+  return (
+    <Helmet>
+      <link
+        rel = 'icon'
+        href = { `https://openweathermap.org/img/wn/${iconID}@2x.png` }
+      />
+
+      <title>{ title }</title>
+    </Helmet>
+  )
+}
